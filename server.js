8c7e3808ed806e3c99527dad41c917226d12e5b6
nnm0122 <nnm0122@gmail.com>
2018-01-23 20:11:52 -0500

    Dependencies set

---
+++ server.js
@@ -0,0 +1,30 @@
+//Dependencies
+var express = express("express");
+var mongojs = require("mongojs");
+var expressHandlebars = ("express-handlebars";)
+var request = require("request");
+var bodyParser = require("body-parser");
+
+
+// Scraping tools
+// Axios is a promised-based http library, similar to jQuery's Ajax method
+// It works on the client and on the server
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+// Require all models
+var db = require("./models");
+
+var PORT = 3000;
+
+// Initialize Express
+var app = express();
+
+// Configure middleware
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Use body-parser for handling form submissions
+app.use(bodyParser.urlencoded({ extended: false }));
+// Use express.static to serve the public folder as a static directory
+app.use(express.static("public"));
